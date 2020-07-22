5ec53826e77062cd3d9888a342bf0c855685ef2e
Jessica Filben <jcfilben@rams.colostate.edu>
2020-07-22 17:41:59 -0600

    Designer header and spacing

--- src/pages/Designer/Designer.js
+++ src/pages/Designer/Designer.js
@@ -25,35 +25,111 @@ const Designer = () => (
   <Box>
     <AppHeader />
     <Main>
-      <Box margin={{ top: 'xlarge' }}>
-        <Stack>
-          <Box>
-            <Box alignSelf="center">
-              <Text
-                margin={{
-                  left: 'large',
-                }}
+      <ResponsiveContext.Consumer>
+        {(responsive) =>
+          responsive === 'large' || responsive === 'xlarge' ? (
+            <Box>
+              <Box height="medium" margin={{ top: 'xlarge' }}>
+                <Stack margin={{ top: 'xlarge' }} anchor="left">
+                  <Box
+                    margin={{ top: 'xlarge' }}
+                    height="gradient1"
+                    background="gradient"
+                  />
+                  <Box margin={{ top: 'large' }} direction="row">
+                    <IconCard
+                      size="medium"
+                      icon={
+                        <ClearOption size="xxlarge" color="DesignerYellow" />
+                      }
+                      margin={{ left: 'xlarge', top: 'large' }}
+                    />
+                    <Box
+                      direction="column"
+                      margin={{
+                        left: 'xlarge',
+                        top: 'xlarge',
+                        bottom: 'medium',
+                      }}
+                    >
+                      <Text> Grommet Tools </Text>
+                      <Heading level={1} size="xlarge" margin={{ top: 'none' }}>
+                        Designer
+                      </Heading>
+                      <Paragraph
+                        margin={{ top: 'none' }}
+                        size="xxlarge"
+                        color="darkGrey"
+                      >
+                        Grommet Designer is a tool used to build expiriences
+                        with Grommet components; then publish and share your
+                        ideas with a simple wysiwg interface
+                      </Paragraph>
+                      <Box
+                        round="medium"
+                        background="buttonYellow"
+                        onClick={() => {}}
+                        width="small"
+                        height="xxsmall"
+                        justify="center"
+                        margin={{ bottom: 'small' }}
+                      >
+                        <Text weight="bold" alignSelf="center" color="black">
+                          Open Designer
+                        </Text>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Stack>
+              </Box>
+              <Box
+                direction="column"
+                gap="medium"
+                margin={{ left: 'xlarge', bottom: 'large' }}
               >
-                Grommet Tools
-              </Text>
-              <Heading
-                size="xlarge"
-                margin={{
-                  top: 'none',
-                  bottom: 'medium',
-                  left: 'large',
-                }}
-              >
-                Designer
-              </Heading>
+                <Box direction="row" margin={{ left: 'xlarge' }} gap="xlarge">
+                  <Camera opacity="0.8" size="large" color="ImagerOrange" />
+                  <Configure opacity="0.9" size="large" color="ThemerOrange" />
+                </Box>
+                <Box margin={{ left: 'xlarge' }} direction="row">
+                  <Box width="xxsmall"></Box>
+                  <CloudUpload
+                    opacity="0.7"
+                    size="large"
+                    color="PublisherPink"
+                  />
+                </Box>
+                <Box margin={{ left: 'xlarge' }} direction="row">
+                  <Box width="xsmall"></Box>
+                  <Selection opacity="0.5" size="large" color="SlidesBlue" />
+                </Box>
+                <Box margin={{ left: 'xlarge' }}>
+                  <BarChart opacity="0.3" size="large" color="TabularGreen" />
+                </Box>
+              </Box>
             </Box>
-            <Box height="gradient1" background="gradient"></Box>
-            <Box alignSelf="center" margin={{ left: 'xlarge' }} width="large">
+          ) : (
+            <Box margin={{ top: 'xlarge' }}>
               <Box margin={{ left: 'xlarge' }}>
-                <Paragraph size="xxlarge" color="darkGrey">
-                  Grommet Designer is a tool used build expiriences with Grommet
-                  components; then publish and share your ideas with a simple
-                  wysiwg interface
+                <Text> Grommet Tools </Text>
+                <Heading
+                  level={1}
+                  size="xlarge"
+                  margin={{ top: 'none', bottom: 'medium' }}
+                >
+                  Designer
+                </Heading>
+              </Box>
+              <Box height="gradient1" background="gradient" />
+              <Box margin={{ left: 'xlarge' }}>
+                <Paragraph
+                  margin={{ top: 'none' }}
+                  size="xxlarge"
+                  color="darkGrey"
+                >
+                  Grommet Designer is a tool used to build expiriences with
+                  Grommet components; then publish and share your ideas with a
+                  simple wysiwg interface
                 </Paragraph>
                 <Box
                   round="medium"
@@ -70,53 +146,9 @@ const Designer = () => (
                 </Box>
               </Box>
             </Box>
-          </Box>
-          <ResponsiveContext.Consumer>
-            {(responsive) =>
-              responsive === 'large' || responsive === 'xlarge' ? (
-                <IconCard
-                  size="medium"
-                  icon={<ClearOption size="xxlarge" color="DesignerYellow" />}
-                  margin={{ left: 'xlarge' }}
-                />
-              ) : responsive === 'medium' ? (
-                <IconCard
-                  size="small"
-                  icon={<ClearOption size="xxlarge" color="DesignerYellow" />}
-                  margin={{ left: 'large' }}
-                />
-              ) : (
-                <IconCard
-                  size="xsmall"
-                  icon={<ClearOption size="xlarge" color="DesignerYellow" />}
-                  margin={{ left: 'medium' }}
-                />
-              )
-            }
-          </ResponsiveContext.Consumer>
-        </Stack>
-      </Box>
-      <Box
-        direction="column"
-        gap="medium"
-        margin={{ left: 'xlarge', bottom: 'xlarge' }}
-      >
-        <Box direction="row" margin={{ left: 'xlarge' }} gap="xlarge">
-          <Camera size="large" color="ImagerOrange" />
-          <Configure size="large" color="ThemerOrange" />
-        </Box>
-        <Box margin={{ left: 'xlarge' }} direction="row">
-          <Box width="xxsmall"></Box>
-          <CloudUpload size="large" color="PublisherPink" />
-        </Box>
-        <Box margin={{ left: 'xlarge' }} direction="row">
-          <Box width="xsmall"></Box>
-          <Selection size="large" color="SlidesBlue" />
-        </Box>
-        <Box margin={{ left: 'xlarge' }}>
-          <BarChart size="large" color="TabularGreen" />
-        </Box>
-      </Box>
+          )
+        }
+      </ResponsiveContext.Consumer>
 
       <Stack>
         <Box
@@ -128,11 +160,12 @@ const Designer = () => (
         ></Box>
         <Box
           margin={{
+            top: 'xlarge',
             horizontal: 'xlarge',
             bottom: 'xlarge',
           }}
         >
-          <Heading size="large" margin={{ top: 'xlarge' }}>
+          <Heading size="large" margin={{ top: 'xlarge', bottom: 'none' }}>
             What is the Designer?
           </Heading>
           <Box width="large" margin={{ bottom: 'xlarge', top: 'none' }}>
@@ -192,9 +225,9 @@ const Designer = () => (
           </Box>
         </Box>
         <Box
-          direction="row"
+          direction="row-responsive"
           justify="center"
-          margin={{ top: 'medium' }}
+          margin={{ top: 'xlarge' }}
           gap="large"
         >
           <ScreenshotCard
