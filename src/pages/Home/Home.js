470f13553663810a85baac3ddffe8d74fb35077a
Joe Butler <joe@Joes-MacBook-Pro.local>
2020-07-24 17:29:41 -0400

    Fixed overhang on Homepage card grid

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,12 @@ import {
   Anchor,
   Box,
   Paragraph,
-  Grid,
   Card,
   CardBody,
   Heading,
   Image,
   Text,
+  Stack,
   ResponsiveContext,
 } from 'grommet';
 import {
@@ -22,7 +22,7 @@ import {
   Github,
   Slack,
 } from 'grommet-icons';
-import GridCard from '../../components/GridCard.js';
+import HomeGrid from '../../components/HomeGrid.js';
 import SmallCard from '../../components/SmallCard.js';
 import AppHeader from '../../components/AppHeader.js';
 
@@ -160,81 +160,44 @@ const Home = (props) => (
       </ResponsiveContext.Consumer>
     </Box>
 
-    <Box background="gradient">
-      <Heading
-        level={2}
-        margin={{
-          horizontal: 'xlarge',
-          top: 'large',
-          bottom: 'small',
-        }}
-        size="xlarge"
-      >
-        Grommet Tool Suite
-      </Heading>
-      <Grid
-        columns={{ count: 'fill', size: 'medium' }}
-        gap="medium"
-        rows="auto"
-        margin={{ horizontal: 'xlarge', vertical: 'small' }}
-      >
-        <GridCard
-          icon={<ClearOption color="DesignerYellow" size="xlarge" />}
-          heading="Open Designer"
-          buttonColor="DesignerYellow"
-          learnLink="/Designer"
-          openLink="https://designer.grommet.io/"
-          text="Grommet web-based WSIWG component editor. Use
-pubilshed themes by entering the URL in the context
-of editing the design details."
-        />
-        <GridCard
-          icon={<Configure size="xlarge" color="ThemerOrange" />}
-          heading="Open Themer"
-          buttonColor="ThemerOrange"
-          openLink="https://theme-designer.grommet.io/"
-          text="Grommet web-based WSIWG component editor. Use
-pubilshed themes by entering the URL in the context
-of editing the design details."
-        />
-        <GridCard
-          icon={<Camera size="xlarge" color="ImagerOrange" />}
-          heading="Open Images"
-          buttonColor="ImagerOrange"
-          openLink="https://images.grommet.io/"
-          text="  Super simple image hosting for your Grommet-based
-projects. Works great with Publisher, Slides, and
-Designer."
-        />
-        <GridCard
-          icon={<CloudUpload size="xlarge" color="PublisherPink" />}
-          heading="Open Publisher"
-          buttonColor="PublisherPink"
-          openLink="https://publisher.grommet.io/"
-          text="Think one part lightweight CMS,
-one part Markdown, and some special sauce
-from Grommet to make it all come together."
-        />
-        <GridCard
-          icon={<Selection size="xlarge" color="SlidesBlue" />}
-          heading="Open Slides"
-          buttonColor="SlidesBlue"
-          openLink="https://slides.grommet.io/"
-          text="Similar to the Grommet Publisher and using
-Markdown and some shortcuts to make presenting easy,
-Slides makes presentations a breeze."
-        />
-        <GridCard
-          icon={<BarChart size="xlarge" color="TabularGreen" />}
-          heading="Open Tabular"
-          buttonColor="TabularGreen"
-          openLink="https://tabular.grommet.io/"
-          text="  If you need a table and have an API or data source,
-choose your columns and create content that is integrated
-with Grommet and the Grommet Designer."
-        />
-      </Grid>
-    </Box>
+    <ResponsiveContext.Consumer>
+      {(responsive) =>
+        responsive === 'large' || responsive === 'xlarge' ? (
+          <Stack>
+            <Box background="gradient" height="1000px"></Box>
+            <Box>
+              <Heading
+                level={2}
+                margin={{
+                  horizontal: 'xlarge',
+                  top: 'large',
+                  bottom: 'small',
+                }}
+                size="xlarge"
+              >
+                Grommet Tool Suite
+              </Heading>
+              <HomeGrid />
+            </Box>
+          </Stack>
+        ) : (
+          <Box background="gradient">
+            <Heading
+              level={2}
+              margin={{
+                horizontal: 'xlarge',
+                top: 'large',
+                bottom: 'small',
+              }}
+              size="xlarge"
+            >
+              Grommet Tool Suite
+            </Heading>
+            <HomeGrid />
+          </Box>
+        )
+      }
+    </ResponsiveContext.Consumer>
 
     <Box
       margin={{
