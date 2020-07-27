4fca6c11b3fe4e6696fa18c1d34c4dd977c9a046
Joe Butler <joe@Joes-MacBook-Pro.local>
2020-07-27 17:05:29 -0400

    Used map on props

--- src/components/common/ScreenShotRow.js
+++ src/components/common/ScreenShotRow.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Box } from 'grommet';
 import ScreenshotCard from '../ScreenshotCard.js';
 
-const ScreenShotRow = ({ margin, src, label }) => (
-  <Box direction="row-responsive" justify="center" margin={margin} gap="large">
-    <ScreenshotCard src={src[0]} label={label[0]} a11yTitle={label[0]} />
-    <ScreenshotCard src={src[1]} label={label[1]} a11yTitle={label[1]} />
-    <ScreenshotCard src={src[2]} label={label[2]} a11yTitle={label[2]} />
+const ScreenShotRow = ({ src, label, ...rest }) => (
+  <Box direction="row-responsive" justify="center" gap="large" {...rest}>
+    {src.map((val, i) => (
+      <ScreenshotCard src={val} label={label[i]} a11yTitle={label[i]} />
+    ))}
   </Box>
 );
 export default ScreenShotRow;
