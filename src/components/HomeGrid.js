c07232fb11968b217eac88f70acefec208812992
Jessica Filben <54560994+jcfilben@users.noreply.github.com>
2020-07-29 15:55:42 -0600

    Home page icon animation (#51)
    * changed home page icon animation and a11yTitle for links
    
    * added href to anchor

--- src/components/HomeGrid.js
+++ src/components/HomeGrid.js
@@ -25,7 +25,8 @@ const HomeGrid = () => (
       heading="Designer"
       buttonName="Open Designer"
       buttonColor="DesignerYellow"
-      learnLink="/Designer"
+      learnLink="/designer"
+      a11yTitle="learn more about Designer"
       openLink="https://designer.grommet.io/"
       text="Grommet web-based WSIWG component editor. Use
       pubilshed themes by entering the URL in the context
@@ -36,6 +37,7 @@ const HomeGrid = () => (
       heading="Themer"
       buttonName="Open Themer"
       buttonColor="ThemerOrange"
+      a11yTitle="learn more about Themer"
       openLink="https://theme-designer.grommet.io/"
       text="Grommet web-based WSIWG component editor. Use
       pubilshed themes by entering the URL in the context
@@ -46,6 +48,7 @@ const HomeGrid = () => (
       heading="Images"
       buttonName="Open Images"
       buttonColor="ImagerOrange"
+      a11yTitle="learn more about Images"
       openLink="https://images.grommet.io/"
       text="  Super simple image hosting for your Grommet-based
       projects. Works great with Publisher, Slides, and
@@ -56,6 +59,7 @@ const HomeGrid = () => (
       heading="Publisher"
       buttonName="Open Publisher"
       buttonColor="PublisherPink"
+      a11yTitle="learn more about Publisher"
       openLink="https://publisher.grommet.io/"
       text="Think one part lightweight CMS,
       one part Markdown, and some special sauce
@@ -71,6 +75,7 @@ const HomeGrid = () => (
       buttonName="Open Slides"
       buttonColor="SlidesBlue"
       learnLink="/Slides"
+      a11yTitle="learn more about Slides"
       openLink="https://slides.grommet.io/"
       text="Similar to the Grommet Publisher and using
       Markdown and some shortcuts to make presenting easy,
@@ -81,6 +86,7 @@ const HomeGrid = () => (
       heading="Tabular"
       buttonName="Open Tabular"
       buttonColor="TabularGreen"
+      a11yTitle="learn more about Tabular"
       openLink="https://tabular.grommet.io/"
       text="  If you need a table and have an API or data source,
       choose your columns and create content that is integrated
