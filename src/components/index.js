38687f7f0ddf4c19a01e1a2f88f99d54b6776c40
Jessica Filben <jcfilben@rams.colostate.edu>
2020-07-29 12:26:31 -0600

    updated imports

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import AppHeader from './AppHeader.js';
+import GridCard from './GridCard.js';
+import HomeGrid from './HomeGrid.js';
+import IconCard from './IconCard.js';
+import ScreenshotCard from './ScreenshotCard.js';
+import SmallCard from './SmallCard.js';
+import CapabilitiesLeft from './common/CapabilitiesLeft.js';
+import CapabilitiesRight from './common/CapabilitiesRight.js';
+import Description from './common/Description.js';
+import HeadingLarge from './common/HeadingLarge.js';
+import HeadingSmall from './common/HeadingSmall.js';
+import RotatedIcon from './common/RotatedIcon.js';
+import ScreenShotRow from './common/ScreenShotRow.js';
+import ToolFooter from './common/ToolFooter.js';
+import ToolHeader from './common/ToolHeader.js';
+
+export {
+  AppHeader,
+  GridCard,
+  HomeGrid,
+  IconCard,
+  ScreenshotCard,
+  SmallCard,
+  CapabilitiesLeft,
+  CapabilitiesRight,
+  Description,
+  HeadingLarge,
+  HeadingSmall,
+  RotatedIcon,
+  ScreenShotRow,
+  ToolFooter,
+  ToolHeader,
+};
