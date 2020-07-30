cd46896bb6ccf360352e07aaca6c9ecf28993b58
Joe Butler <jbutler5510@gmail.com>
2020-07-30 18:57:09 -0400

    Adds Themer page (#41)
    * Small changes
    
    * Added Themer page
    
    * Updated link to themer on homepag
    
    * Fixed href
    
    * FIxed svgs
    
    * Updaed svg
    
    * Updaed svg
    
    * Fixed round on second svg in capabilities
    
    * Updated svgs
    
    * Fixed how capabilities heading is aligned on resize
    
    * Fixed duplicate prop
    
    * Remove redundant box
    
    * Heading responsive in Capabilities right
    
    * Updated imports
    
    Co-authored-by: Joe Butler <joe@Joes-MacBook-Pro.local>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Feedback from './pages/Feedback/Feedback.js';
 import Designer from './pages/Designer/Designer.js';
 import Slides from './pages/Slides/Slides.js';
 import Tabular from './pages/Tabular/Tabular.js';
+import Themer from './pages/Themer/Themer.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { grommetToolsTheme } from './theme.js';
 import { Grommet } from 'grommet';
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route path="/slides">
             <Slides />
+          </Route>
+          <Route path="/themer">
+            <Themer />
           </Route>
           <Route path="/feedback">
             <Feedback />
