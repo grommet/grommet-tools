4cde6d2349896d20b1df74567b251883b6b0723b
Joe Butler <jbutler5510@gmail.com>
2020-08-05 14:04:40 -0400

    Makes Nav collapse into hamburger menu on small window (#88)
    * Collapsible Nav
    
    * Fixed hamburger styling
    
    * Merge branch 'master' of https://github.com/grommet/grommet-tools into fix/navHamburger
    
    * Small changes
    
    * Added a11yTitle
    
    Co-authored-by: Joe Butler <joe@Joes-MacBook-Pro.local>

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import { Header, Anchor, Box } from 'grommet';
-import { Grommet as GrommetIcon } from 'grommet-icons';
+import { Header, Anchor, Box, ResponsiveContext, Menu } from 'grommet';
+import { Grommet as GrommetIcon, Menu as MenuIcon } from 'grommet-icons';
 
 const AppHeader = () => (
-  <Header height="xsmall">
-    <Box justify="start" direction="row" gap="small">
-      <Box margin={{ left: 'medium' }} align="center">
-        <GrommetIcon size="customSmall" color="brand" />
-      </Box>
-      <Anchor margin="xsmall" href="/" label="Grommet Tools" />
-    </Box>
+  <ResponsiveContext.Consumer>
+    {(responsive) =>
+      responsive === 'small' || responsive === 'xsmall' ? (
+        <Header height="xsmall">
+          <Box
+            margin={{ left: 'large' }}
+            align="start"
+            onClick={() => {
+              window.open('/', '_self');
+            }}
+          >
+            <GrommetIcon size="customSmall" color="brand" />
+          </Box>
+          <Box alignSelf="end" margin="large">
+            <Menu
+              a11yTitle="Navigation Menu"
+              justifyContent="end"
+              dropProps={{ align: { top: 'bottom', right: 'right' } }}
+              icon={<MenuIcon color="brand" size="customSmall" />}
+              pad="small"
+              items={[
+                { label: <Box pad="small">Grommet Tools </Box>, href: '/' },
+                {
+                  label: <Box pad="small">Grommet.io</Box>,
+                  href: 'https://v2.grommet.io',
+                },
+                {
+                  label: <Box pad="small">Feedback</Box>,
+                  href: '/feedback',
+                },
+              ]}
+            />
+          </Box>
+        </Header>
+      ) : (
+        <Header height="xsmall">
+          <Box justify="start" direction="row" gap="small">
+            <Box
+              margin={{ left: 'medium' }}
+              align="center"
+              onClick={() => {
+                window.open('/', '_self');
+              }}
+            >
+              <GrommetIcon size="customSmall" color="brand" />
+            </Box>
+            <Anchor margin="xsmall" href="/" label="Grommet Tools" />
+          </Box>
 
-    <Box margin={{ right: 'large' }} justify="end" direction="row" gap="medium">
-      <Anchor
-        href="https://v2.grommet.io/"
-        target="_blank"
-        label="Grommet.io"
-      />
-      <Anchor href="/feedback" label="Feedback" />
-    </Box>
-  </Header>
+          <Box
+            margin={{ right: 'large' }}
+            justify="end"
+            direction="row"
+            gap="medium"
+          >
+            <Anchor
+              href="https://v2.grommet.io/"
+              label="Grommet.io"
+              target="_blank"
+            />
+            <Anchor href="/feedback" label="Feedback" />
+          </Box>
+        </Header>
+      )
+    }
+  </ResponsiveContext.Consumer>
 );
 export { AppHeader };
